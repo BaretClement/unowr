412d8b65c3bf5a2c0d21a1bffdef844f0076cb79
Clément Baret <clement.baret@gmail.com>
2016-10-27 10:33:17 +0200

    Update home theme

--- wp-content/themes/minnow-child/template-concept.php
+++ wp-content/themes/minnow-child/template-concept.php
@@ -1,50 +1,37 @@
 <?php
 /*
-Template Name: Concept UNOWR
+Template Name: Concept
 */
 ?>
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 <head>
-	<title>UNOWR | Trouvez le restaurant qu'il vous faut !</title>
-	<!-- meta -->
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="Avec UNOWR, trouvez le restaurant qui vous convient en 7 questions. Rapide et efficace Nous nous adressons aussi bien aux restaurateurs qu'aux consommateurs !">
-	<meta name="author" content="UNOWR Team">
 
-	<!-- links -->
-	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
-	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/materialize.min.css">
-	<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/css/img/placeholder.png"/>
-	<link rel="stylesheet" type="text/css" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-    
-	<!-- fonts -->
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans:800" rel="stylesheet">
-	<link href='https://fonts.googleapis.com/css?family=Lato:400,100,100italic,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+  <meta charset="utf-8">
+  <title>UNOWR | Trouvez le restaurant qu'il vous faut !</title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Raleway:200,300,400,500,700,900" rel="stylesheet">
+  <!-- css -->
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/header.css">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/normalize.css">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/skeleton.css">
+  <link rel="stylesheet" type="text/css" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- js -->
+  <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/script.js"></script>
+  <!-- favicon -->
+  <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/css/img/placeholder.png">
 
-	<!-- captcha Google-->
-	<script src="https://www.google.com/recaptcha/api.js" async defer></script>
 </head>
 <body>
 
-	
 
-	<!-- JQUERY -->
-	<script async type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-	<script async src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-	<!-- Materialize & script custom -->
-	<script async src="<?php echo get_template_directory_uri(); ?>/js/materialize.min.js"></script>
-	<script async src="<?php echo get_template_directory_uri(); ?>/js/script.js"></script>
-	<!-- Google Analytics -->
-	<script>
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-	  ga('create', 'UA-86120720-1', 'auto');
-	  ga('send', 'pageview');
-	</script>
+	<!-- start content part -->
+		
+	<!-- end content part -->
 
 </body>
 </html>
--- wp-content/themes/minnow-child/template-homepage.php
+++ wp-content/themes/minnow-child/template-homepage.php
@@ -8,13 +8,14 @@ Template Name: Homepage
 <head>
 
   <meta charset="utf-8">
-  <title>Your page title here :)</title>
+  <title>UNOWR | Trouvez le restaurant qu'il vous faut !</title>
   <meta name="description" content="">
   <meta name="author" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- fonts -->
   <link href="https://fonts.googleapis.com/css?family=Raleway:200,300,400,500,700,900" rel="stylesheet">
   <!-- css -->
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/header.css">
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/normalize.css">
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/skeleton.css">
@@ -22,79 +23,113 @@ Template Name: Homepage
   <!-- js -->
   <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/script.js"></script>
   <!-- favicon -->
-  <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/css/img/favicon.png">
+  <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/css/img/placeholder.png">
 
 </head>
 <body>
 
-  <nav></nav>
-	<!-- content part -->
+	<!-- start content part -->
 	<div class="container">
-    
-    <!-- user part -->
+    <!-- start user part -->
     <div class="row">
-      <div class="twelve columns user">
+      <div class="twelve columns user <?php the_field('couleur_texte_utilisateur'); ?>"h>
         <div class="container content">
-          <h2>Trouvez le restaurant qu'il vous faut !</h2>
-          <p>Ne perdez plus de temps, répondez aux questions & trouvez le restaurant !</p>
-          <button>GO !</button>
+          <h2><?php the_field('titre_utilisateur'); ?></h2>
+          <p><?php the_field('sous_titre_utilisateur'); ?></p>
+          <button class="<?php the_field('couleur_texte_utilisateur'); ?>"><?php the_field('cta_utilisateur'); ?> <span class="<?php the_field('pictogramme_utilisateur'); ?>"></span></button>
         </div>
       </div>
     </div>
+    <!-- end user part -->
     
-    <!-- restaurant part -->
+    <!-- start restaurant part -->
     <div class="row">
-      <div class="twelve columns restaurant">
-        <div class="container content">
-          <h2>Inscrivez votre restaurant</h2>
-          <p>Vous souhaitez faire partie de l'avanture ? Rejoignez-nous !</p>
-          <button>S'inscrire :)</button>
+      <div class="twelve columns restaurant <?php the_field('couleur_texte_restaurant'); ?>">
+        <div class="container content hvr-hollow">
+          <h2><?php the_field('titre_restaurateur'); ?></h2>
+          <p><?php the_field('sous_titre_restaurateur'); ?></p>
+          <button class="<?php the_field('couleur_texte_restaurant'); ?>"><?php the_field('cta_restaurateur'); ?> <span class="<?php the_field('pictogramme_restaurateur'); ?>"></span></button>
         </div>
       </div>
     </div>
+    <!-- end restaurant part -->
 
-    
+    <!-- start blog & concept part -->
     <div class="row no-margin-bottom">
 
-      <!-- blog part -->
-      <div class="six columns blog">
+      <!-- strat blog part -->
+      <div class="six columns blog <?php the_field('couleur_texte_blog'); ?>">
         <div class="container content">
-          <h3>Blog</h3>
-          <p>Suivez nos découvertes et nos actualités</p>
-          <button>Suivre...</button>
+          <h2><?php the_field('titre_blog'); ?></h2>
+          <p><?php the_field('sous_titre_blog'); ?></p>
+          <button class="<?php the_field('couleur_texte_blog'); ?>"><?php the_field('cta_blog'); ?> <span class="<?php the_field('pictogramme_blog'); ?>"></span></button>
         </div>
       </div>
+      <!-- end blog part -->
 
-
-      <!-- concept part -->
-      <div class="six columns concept">
+      <!-- start concept part -->
+      <div class="six columns concept <?php the_field('couleur_texte_concept'); ?>">
         <div class="container content">
-          <h3>Concept</h3>
-          <p>Vous voulez nous connaitre ?</p>
-          <button>Découvrir !</button>
+          <h2><?php the_field('titre_concept'); ?></h2>
+          <p><?php the_field('sous_titre_concept'); ?></p>
+          <button class="<?php the_field('couleur_texte_concept'); ?>"><?php the_field('cta_concept'); ?> <span class="<?php the_field('pictogramme_concept'); ?>"></span></button>
         </div>
       </div>
+      <!-- end concept part -->
 
     </div>
+    <!-- end blog & concept part -->
 
-    <!-- social part -->
+    <!-- start social part -->
     <div class="row">
-      <div class="twelve columns social">
+      <div class="twelve columns social <?php the_field('couleur_texte_social'); ?>">
         <div class="container content">
-          <h3>Suivez-nous sur les réseaux sociaux !</h3>
-          <h3>
-          <span class="ion-social-instagram-outline margin-icon"></span>
-          <span class="ion-social-facebook-outline margin-icon"></span>
-          <span class="ion-social-twitter-outline margin-icon"></span>
-          <span class="ion-social-snapchat-outline margin-icon"></span>
+          <h2><?php the_field('titre_social'); ?></h2>
+          <p><?php the_field('sous_titre_social'); ?></p>
+          <h3 class="no-margin-bottom">
+          <span class="<?php the_field('pictogramme_social_1'); ?> margin-icon hvr-grow"></span>
+          <span class="<?php the_field('pictogramme_social_2'); ?> margin-icon hvr-grow"></span>
+          <span class="<?php the_field('pictogramme_social_3'); ?> margin-icon hvr-grow"></span>
+          <span class="<?php the_field('pictogramme_social_4'); ?> margin-icon hvr-grow"></span>
           </h3>
-          
-          
         </div>
       </div>
     </div>
+    <!-- end social part -->
+
+    <!-- start photos part -->
+    <div class="row">
+
+      <!-- strat photo 1 part -->
+      <div class="six columns photo1">
+        <div class="container content">
+          <img class="responsive-img" src="<?php the_field('photo_1'); ?>">
+        </div>
+      </div>
+      <!-- start photo 2 part -->
+      <div class="six columns photo2">
+        <div class="container content">
+          <img class="responsive-img" src="<?php the_field('photo_2'); ?>">
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <!-- start photo 3 part -->
+      <div class="six columns photo3">
+        <div class="container content">
+          <img class="responsive-img" src="<?php the_field('photo_3'); ?>">
+        </div>
+      </div>
+      <!-- start photo 4 part -->
+      <div class="six columns photo4">
+        <div class="container content">
+          <img class="responsive-img" src="<?php the_field('photo_4'); ?>">
+        </div>
+      </div>
 
   </div>
+  <!-- end content part -->
 
 </body>
 </html>

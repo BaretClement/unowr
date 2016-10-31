8b4509d35045f01e243ec17f782d63fc442973a0
Clément Baret <clement.baret@gmail.com>
2016-10-31 13:08:49 +0100

    Update theme & plugins

---
+++ wp-content/themes/minnow-child/template-article.php
@@ -0,0 +1,44 @@
+<?php
+/*
+Template Name: Concept
+*/
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+
+  <meta charset="utf-8">
+  <title>UNOWR | Un service rapide et pratique !</title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Raleway:200,300,400,500,700,900" rel="stylesheet">
+  <!-- css -->
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/header.css">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/normalize.css">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/skeleton.css">
+  <link rel="stylesheet" type="text/css" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- js -->
+  <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/script.js"></script>
+  <!-- favicon -->
+  <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/css/img/placeholder.png">
+
+</head>
+<body>
+  <div class="container article">
+  <!-- Start banner -->
+  <!-- End banner-->
+
+  <!-- Start title -->
+  <!-- End title-->
+
+  <!-- Start content -->
+  <!-- End content-->
+
+  <!-- Start info restaurant -->
+  <!-- End info restaurant-->
+  </div>
+</body>
+</html>

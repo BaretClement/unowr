955d89029c2aac3f497a343a594f22699fb44d48
Clément Baret <clement.baret@gmail.com>
2016-11-02 18:08:53 +0100

    Udpate theme - header

--- wp-content/themes/minnow-child/template-article.php
+++ wp-content/themes/minnow-child/template-article.php
@@ -2,31 +2,8 @@
 /*
 Template Name: Concept
 */
-?>
-<!DOCTYPE html>
-<html lang="fr">
-<head>
+get_header( 'custom' ); ?>
 
-  <meta charset="utf-8">
-  <title>UNOWR | Un service rapide et pratique !</title>
-  <meta name="description" content="">
-  <meta name="author" content="">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Raleway:200,300,400,500,700,900" rel="stylesheet">
-  <!-- css -->
-  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/header.css">
-  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
-  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/normalize.css">
-  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/skeleton.css">
-  <link rel="stylesheet" type="text/css" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-  <!-- js -->
-  <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/script.js"></script>
-  <!-- favicon -->
-  <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/css/img/placeholder.png">
-
-</head>
-<body>
   <div class="container article">
   <!-- Start banner -->
   <!-- End banner-->

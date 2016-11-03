a62e9c3bbc1a586360dffd9f1c38337a76b75ac7
Clément Baret <clement.baret@gmail.com>
2016-11-03 13:47:15 +0100

    Deleted useless taxonomy

--- wp-content/plugins/unowr-search/views/form.php
+++ wp-content/plugins/unowr-search/views/form.php
@@ -2,7 +2,7 @@
 
 <?php
 // $taxonomies = get_taxonomies();
-$taxonomies = array('ambiance', 'localisation', 'occasion', 'type_de_cuisine', 'nombre_de_personnes', 'agenda', 'mise_en_avant');
+$taxonomies = array('ambiance', 'localisation', 'occasion', 'type_de_cuisine', 'nombre_de_personnes', 'agenda');
 $first = true;
 foreach ($taxonomies as $tkey => $taxonomy) {
 

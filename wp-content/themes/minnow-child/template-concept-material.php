919e57923aaedd153886edbf67547f3cb475aa4c
Clément Baret <clement.baret@gmail.com>
2016-11-05 15:48:24 +0100

    Update theme template

---
+++ wp-content/themes/minnow-child/template-concept-material.php
@@ -0,0 +1,129 @@
+<?php
+/*
+Template Name: Concept - material
+*/
+
+get_header( 'custom-material' ); ?>
+
+  <!-- BLOC UNOWR -->
+  <div class="row <?php the_field('couleur_texte_unowr'); ?>" style="background-color: <?php the_field('background_unowr'); ?>; background-img: url(<?php the_field('background_image_unowr'); ?>);">
+    <div class="col s12 height100 valign-wrapper">
+      <div class="container valign center">
+        <h1><?php the_field('titre_unowr'); ?></h1>
+        <p><?php the_field('sous_titre_unowr'); ?></p>
+      </div>
+    </div>
+  </div>
+
+  <!-- BLOC IDEA -->
+  <div class="row <?php the_field('couleur_texte_idea'); ?>" style="background-color: <?php the_field('background_idea'); ?>; background-image: url(<?php the_field('background_image_idea'); ?>);">
+    <div class="col s12 height100 valign-wrapper">
+      <div class="container valign center">
+        <h2><?php the_field('titre_idea'); ?></h2>
+        <p><?php the_field('sous_titre_idea') ?></p>
+        <p><?php the_field('contenu_idea') ?></p>
+      </div>
+    </div>
+  </div>
+
+  <!-- BLOC PRINCIPLE -->
+  <div class="row <?php the_field('couleur_texte_principle'); ?>" style="background-color: <?php the_field('background_principle'); ?>; background-image: url(<?php the_field('background_image_principle'); ?>);">
+      
+    <div class="col s12 valign-wrapper">
+      <div class="container valign center">
+        <h2><?php the_field('titre_principle') ?></h2>
+        <p><?php the_field('sous_titre_principle') ?></p>
+      </div>
+    </div>
+    
+      <div class="col s12 l4 valign-wrapper">
+        <div class="container principle valign center">
+          <i class="big-icon <?php the_field('pictogramme_principle_1') ?>"></i>
+          <h3><?php the_field('titre_principle_1') ?></h3>
+          <p><?php the_field('contenu_principle_1') ?></p>
+        </div>
+      </div>
+
+      <div class="col s12 l4 valign-wrapper">
+        <div class="container principle valign center">
+          <i class="big-icon <?php the_field('pictogramme_principle_2') ?>"></i>
+          <h3><?php the_field('titre_principle_2') ?></h3>
+          <p><?php the_field('contenu_principle_2') ?></p>
+        </div>
+      </div>
+
+      <div class="col s12 l4 valign-wrapper">
+        <div class="container principle valign center">
+          <i class="big-icon <?php the_field('pictogramme_principle_3') ?>"></i>
+          <h3><?php the_field('titre_principle_3') ?></h3>
+          <p><?php the_field('contenu_principle_3') ?></p>
+        </div>
+      </div>
+
+  </div>
+
+  <!-- BLOC OBJECTIVES -->
+  <div class="row <?php the_field('couleur_texte_objective'); ?>" style="background-color: <?php the_field('background_objective'); ?>; background-image: url(<?php the_field('background_image_objective'); ?>);">  
+    
+    <div class="col s12 valign-wrapper">
+      <div class="container valign center">
+        <h2><?php the_field('titre_objectif') ?></h2>
+        <p><?php the_field('sous_titre_objectif') ?></p>
+      </div>
+    </div>
+
+    <div class="col s12 l6 height33 valign-wrapper top-left">
+      <div class="container objective valign">
+        <h3><?php the_field('titre_objectif_1') ?></h3>
+        <p><?php the_field('contenu_objectif_1') ?></p>
+      </div>
+    </div>
+
+    <div class="col s12 l6 height33 valign-wrapper top-right">
+      <div class="container objective valign">
+        <h3><?php the_field('titre_objectif_2') ?></h3>
+        <p><?php the_field('contenu_objectif_2') ?></p>
+      </div>
+    </div>
+
+    <div class="col s12 l6 height33 valign-wrapper bottom-left">
+      <div class="container objective valign">
+        <h3><?php the_field('titre_objectif_3') ?></h3>
+        <p><?php the_field('contenu_objectif_3') ?></p>
+      </div>
+    </div>
+
+    <div class="col s12 l6 height33 valign-wrapper bottom-right">
+      <div class="container objective valign">
+        <h3><?php the_field('titre_objectif_4') ?></h3>
+        <p><?php the_field('contenu_objectif_4') ?></p>
+      </div>
+    </div>
+
+  </div>
+
+  <!-- BLOC TEAM -->
+  <div class="row <?php the_field('couleur_texte_equipe'); ?>" style="background-color: <?php the_field('background_equipe'); ?>; background-image: url(<?php the_field('background_image_equipe'); ?>);"> 
+    <div class="col s12 height100 valign-wrapper">
+      <div class="container valign center">
+        <h2><?php the_field('titre_equipe') ?></h2>
+        <p><?php the_field('sous_titre_equipe') ?></p>
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+
+
+
+
+<?php the_field('pictogramme_idea') ?>
+<?php the_field('pictogramme_principle') ?>
+<?php the_field('pictogramme_objectif') ?>
+<?php the_field('pictogramme_equipe') ?>
+
+
+
--- wp-content/themes/minnow-child/template-homepage-material.php
+++ wp-content/themes/minnow-child/template-homepage-material.php
@@ -6,7 +6,7 @@ Template Name: Homepage - material
 get_header( 'custom-material' ); ?>
 
 	<!-- BLOC UTILISATEUR -->
-	<div id="utilisateur" class="row <?php the_field('couleur_texte_utilisateur') ?>" style="background-image: url(<?php the_field('background_image_utilisateur'); ?>);">
+	<div id="utilisateur" class="row <?php the_field('couleur_texte_utilisateur') ?>" style="background-color: <?php the_field('background_utilisateur'); ?>; background-image: url(<?php the_field('background_image_utilisateur'); ?>);">
 		<div class="col s12 height100 valign-wrapper">
 			<div class="container valign center">
 				<h1><?php the_field('titre_utilisateur'); ?></h1>
@@ -19,7 +19,7 @@ get_header( 'custom-material' ); ?>
 	</div>
 
 	<!-- BLOC RESTAURATEUR -->
-	<div class="row <?php the_field('couleur_texte_restaurant') ?>" style="background-image: url(<?php the_field('background_image_restaurateur'); ?>);">
+	<div class="row <?php the_field('couleur_texte_restaurant') ?>" style="background-color: <?php the_field('background_restaurateur'); ?>; background-image: url(<?php the_field('background_image_restaurateur'); ?>);">
 		<div class="col s12 height100 valign-wrapper">
 			<div class="container valign center">
 				<h2><?php the_field('titre_restaurateur'); ?></h2>
@@ -35,7 +35,7 @@ get_header( 'custom-material' ); ?>
 	<div class="row">
 
 		<!-- BLOC BLOG -->
-		<div id="blog" class="col s12 l6 height75 valign-wrapper <?php the_field('couleur_texte_blog') ?>" style="background-image: url(<?php the_field('background_image_blog'); ?>);">
+		<div id="blog" class="col s12 l6 height75 valign-wrapper <?php the_field('couleur_texte_blog') ?>" style="background-color: <?php the_field('background_blog'); ?>; background-image: url(<?php the_field('background_image_blog'); ?>);">
 			<div class="container valign center">
 				<h2><?php the_field('titre_blog'); ?></h2>
 				<p><?php the_field('sous_titre_blog'); ?></p>
@@ -46,7 +46,7 @@ get_header( 'custom-material' ); ?>
 		</div>
 
 		<!-- BLOC CONCEPT -->
-		<div class="col s12 l6 height75 valign-wrapper <?php the_field('couleur_texte_concept') ?>" style="background-image: url(<?php the_field('background_image_concept'); ?>);">
+		<div class="col s12 l6 height75 valign-wrapper <?php the_field('couleur_texte_concept') ?>" style="background-color: <?php the_field('background_concept'); ?>; background-image: url(<?php the_field('background_image_concept'); ?>);">
 			<div class="container valign center">
 				<h2><?php the_field('titre_concept'); ?></h2>
 				<p><?php the_field('sous_titre_concept'); ?></p>
@@ -58,7 +58,7 @@ get_header( 'custom-material' ); ?>
 	</div>
 
 	<!-- BLOC SOCIAL -->
-	<div class="row <?php the_field('couleur_texte_social') ?>" style="background-image: url(<?php the_field('background_image_social'); ?>);">
+	<div class="row <?php the_field('couleur_texte_social') ?>" style="background-color: <?php the_field('background_social'); ?>; background-image: url(<?php the_field('background_image_social'); ?>);">
 		<div class="col s12 height50 valign-wrapper">
 			<div class="container center valign">	
 				<h2><?php the_field('titre_social'); ?></h2>

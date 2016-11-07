d60485c4dd937ce2bd7fae8fa894e761976a69da
Clément Baret <clement.baret@gmail.com>
2016-11-07 13:48:20 +0100

    Add form

--- wp-content/themes/minnow-child/template-restaurateur-material.php
+++ wp-content/themes/minnow-child/template-restaurateur-material.php
@@ -5,152 +5,246 @@ Template Name: Restaurateur - material
 
 get_header( 'custom-material' ); ?>
 
-	<!-- Contact -->
-  	<div id="form" class="row padding-vertical valign-wrapper background-black color-white">
-  		<div class="container valign">
-  			<h2 class="align-center">Contactez-nous <sup class="ion-paper-airplane color-yellow"></sup></h2>
-  			<p class="align-center margin-bottom">Vous êtes restaurateur et notre concept vous plait ?<br> Vous avez la possibilité de nous contacter et même de faire partie de nos premiers partenaires !</p>
-	    	<!-- form -->
-	    	<form class="col s12 color-white" action="#contact" method="POST" autocomplete="off">
-	      		<h4>À propos de vous</h4>
-	      		<div class="row">
-	        		
-			        <div class="input-field col s12 m6">
-			          	<input name="first_name" id="last_name" type="text" class="validate" required="required">
-			          	<label for="first_name">Prénom</label>
-			        </div>
+<div class="container formulaire">
+	<form>
+		<div class="row">
+			<!-- TITLE -->
+			<div class="col s12"><h1 class="center">Formulaire d'inscription</h1></div>
 
-	        		<div class="input-field col s12 m6">
-	          			<input name="last_name" id="first_name" type="text" class="validate" required="required">
-	          			<label for="last_name">Nom</label>
-	        		</div>
-	        		
+			<!-- NOM DU RESTAURANT -->
+	        <div class="input-field col s12">
+	          	<input id="nom_du_restaurant" type="text" class="validate" required="">
+	          	<label for="nom_du_restaurant">Nom du restaurant</label>
+	        </div>
+			<!-- ADRESSE DU RESTAURANT -->
+	        <div class="input-field col s12 m6">
+	          	<input id="adresse" type="text" class="validate">
+	          	<label for="adresse">Adresse</label>
+	        </div>
+			<!-- CODE POSTAL DU RESTAURANT -->
+	        <div class="input-field col s6 m3">
+	          	<input id="code_postal" type="text" class="validate">
+	          	<label for="code_postal">Code postal</label>
+	        </div>
+			<!-- VILLE DU RESTAURANT -->
+	        <div class="input-field col s6 m3">
+	          	<input id="ville" type="text" class="validate">
+	          	<label for="ville">Ville</label>
+	        </div>
+			<!-- TÉLÉPHONE -->
+	        <div class="input-field col s6">
+	          	<input id="telephone" type="text" class="validate">
+	          	<label for="telephone">Téléphone</label>
+	        </div>
+			<!-- EMAIL -->
+	        <div class="input-field col s6">
+	          	<input id="email" type="email" class="validate">
+	          	<label for="email">Email</label>
+	        </div>
+			<!-- PRÉNOM DU CONTACT -->
+	        <div class="input-field col s6">
+	          	<input id="prenom_du_contact" type="text" class="validate">
+	          	<label for="prenom_du_contact">Prénom du contact</label>
+	        </div>
+			<!-- NOM DU CONTACT -->
+	        <div class="input-field col s6">
+	          	<input id="nom_du_contact" type="text" class="validate">
+	          	<label for="nom_du_contact">Nom du contact</label>
+	        </div>
+			<!-- START HORAIRES D'OUVERTURE -->
+	        <div class="col s12">
+	        	<table class="formulaire">
+        			<thead>
+          				<tr>
+				            <th data-field="id">Horaires d'ouverture ( à cocher )</th>
+				        </tr>
+        			</thead>
+        			<tbody>
+        				<!-- lundi -->
+          				<tr>
+            				<td>Lundi</td>
+            				<td>      
+            					<input type="checkbox" class="filled-in" id="lundi_midi"/>
+      							<label for="lundi_midi">midi</label>
+      						</td>
+            				<td>      
+            					<input type="checkbox" class="filled-in" id="lundi_soir"/>
+            					<label for="lundi_soir">soir</label>
+            				</td>
+          				</tr>
+        				<!-- mardi -->
+          				<tr>
+            				<td>Mardi</td>
+            				<td>      
+            					<input type="checkbox" class="filled-in" id="mardi_midi"/>
+      							<label for="mardi_midi">midi</label>
+      						</td>
+            				<td>      
+            					<input type="checkbox" class="filled-in" id="mardi_soir"/>
+            					<label for="mardi_soir">soir</label>
+            				</td>
+          				</tr>
+						<!-- mercredi -->
+          				<tr>
+            				<td>Mercredi</td>
+            				<td>      
+            					<input type="checkbox" class="filled-in" id="mercredi_midi"/>
+      							<label for="mercredi_midi">midi</label>
+      						</td>
+            				<td>      
+            					<input type="checkbox" class="filled-in" id="mercredi_soir"/>
+            					<label for="mercredi_soir">soir</label>
+            				</td>
+          				</tr>
+						<!-- jeudi -->
+          				<tr>
+            				<td>Jeudi</td>
+            				<td>      
+            					<input type="checkbox" class="filled-in" id="jeudi_midi"/>
+      							<label for="jeudi_midi">midi</label>
+      						</td>
+            				<td>      
+            					<input type="checkbox" class="filled-in" id="jeudi_soir"/>
+            					<label for="jeudi_soir">soir</label>
+            				</td>
+          				</tr>
+						<!-- vendredi -->
+          				<tr>
+            				<td>Vendredi</td>
+            				<td>      
+            					<input type="checkbox" class="filled-in" id="vendredi_midi"/>
+      							<label for="vendredi_midi">midi</label>
+      						</td>
+            				<td>      
+            					<input type="checkbox" class="filled-in" id="vendredi_soir"/>
+            					<label for="vendredi_soir">soir</label>
+            				</td>
+          				</tr>
+						<!-- samedi -->
+          				<tr>
+            				<td>Samedi</td>
+            				<td>      
+            					<input type="checkbox" class="filled-in" id="samedi_midi"/>
+      							<label for="samedi_midi">midi</label>
+      						</td>
+            				<td>      
+            					<input type="checkbox" class="filled-in" id="samedi_soir"/>
+            					<label for="samedi_soir">soir</label>
+            				</td>
+          				</tr>
+						<!-- dimanche -->
+          				<tr>
+            				<td>Dimanche</td>
+            				<td>      
+            					<input type="checkbox" class="filled-in" id="dimanche_midi"/>
+      							<label for="dimanche_midi">midi</label>
+      						</td>
+            				<td>      
+            					<input type="checkbox" class="filled-in" id="dimanche_soir"/>
+            					<label for="dimanche_soir">soir</label>
+            				</td>
+          				</tr>
+					</tbody>
+				</table>
+	        </div>
+	        <!-- END HORAIRES D'OUVERTURE -->
+	        <!-- START TYPE DE CUISINE -->
+       		<div class="input-field col s12">
+		    	<select multiple>
+		    		<option value="" disabled selected>Type de cuisine ( à cocher )</option>
+		      		<option value="1">Végétarien</option>
+		      		<option value="1">Poissons & fruits de mer</option>
+		      		<optgroup label="Cuisine asiatique"></optgroup>
+		      		<option value="1">Japonais</option>
+		      		<option value="1">Chinois</option>
+		      		<option value="1">Wok</option>
+		      		<option value="1">Indien</option>
+		      		<option value="1">Thaï</option>
+		      		<option value="1">Vietnamien</option>
+		      		<option value="1">Malaisien</option>
+		      		<option value="1">Libanais</option>
+		      		<optgroup label="Cuisine nord-américaine"></optgroup>
+		      		<option value="1">Burger</option>
+		      		<option value="1">Bagel</option>
+		      		<option value="1">Cubain</option>
+		      		<option value="1">Mexicain</option>
+		      		<optgroup label="Cuisine africaine"></optgroup>
+		      		<option value="1">Couscous & tajines</option>
+		      		<option value="1">Turc</option>
+		    	</select>
+		  	</div>
+	        <div class="input-field col s12">
+	          	<input id="specialite" type="text" class="validate">
+	          	<label for="specialite">Vous n'avez pas trouvé votre type de cuisine ? Indiquez le ici :)</label>
+	        </div>
+	        <!-- END TYPE DE CUISINE -->
 
-			        <div class="input-field col s12 m6">
-			        	<i class="ion-ios-telephone-outline prefix"></i>
-			          	<input name="telephone" id="telephone" type="tel" class="validate" maxlength="10" pattern="^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,5})|(\(?\d{2,6}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$" required="required">
-			          	<label id="telephone" for="telephone" data-error="Numéro invalide">Téléphone</label>
-			        </div>
+			<!-- SPECIALITÉ -->
+	        <div class="input-field col s6">
+	          	<input id="specialite" type="text" class="validate">
+	          	<label for="specialite">Spécialité du restaurant</label>
+	        </div>
+			<!-- PRIX MOYEN -->
+	        <div class="input-field col s6">
+	          	<input id="prix_moyen" type="text" class="validate">
+	          	<label for="prix_moyen">Prix moyen ( entrée / plat / dessert )</label>
+	        </div>
+			<!-- START AMBIANCE -->
+			<div class="col s12">
+	        	<table class="formulaire">
+        			<thead>
+          				<tr>
+				            <th data-field="id">Type d'ambiance ( à cocher )</th>
+				        </tr>
+        			</thead>
+        			<tbody>
+          				<tr>
+            				<td>      
+            					<input type="checkbox" class="filled-in" id="lumiere_tamisee"/>
+      							<label for="lumiere_tamisee">Lumière tamisée</label>
+      						</td>
+            				<td>      
+            					<input type="checkbox" class="filled-in" id="chill"/>
+            					<label for="chill">Chill</label>
+            				</td>
+            				<td>      
+            					<input type="checkbox" class="filled-in" id="costume_trois_pièces"/>
+            					<label for="costume_trois_pièces">Costume trois pièces</label>
+            				</td>
+            			</tr>
+            			<tr>
+            				<td>      
+            					<input type="checkbox" class="filled-in" id="bonne_franquette"/>
+            					<label for="bonne_franquette">Bonne franquette</label>
+            				</td>
+            				<td>      
+            					<input type="checkbox" class="filled-in" id="rdv_en_terre_inconnue"/>
+            					<label for="rdv_en_terre_inconnue">Rendez-vous en terre inconnue</label>
+            				</td>
+							<td>      
+            					<input type="checkbox" class="filled-in" id="bon_classique"/>
+            					<label for="bon_classique">Bon classique</label>
+            				</td>
+          				</tr>
+          			</tbody>
+          		</table>
+          	</div>
+          	<!-- END AMBIANCE -->
+          	<!-- BOUTON ENVOYER -->
+          	<div class="col s12 center margin-bottom">
+				<button class="btn-flat waves-effect waves-light" type="submit" name="action">
+					Envoyer
+  				</button>
+			</div>
+		</form>
+	</div>
 
-			        <div class="input-field col s12 m6">
-			        	<i class="ion-ios-at-outline prefix"></i>
-			          	<input name="email" id="email" type="email" class="validate" required="required" autocomplete="off">
-			          	<label for="email" data-error="Email invalide">Email</label>
-			        </div>
 
-				</div>
 
-				<h4>À propos de votre restaurant</h4>
-				<div class="row">
-			        <div class="input-field col s12">
-			        <i class="ion-ios-home-outline prefix"></i>
-			          	<input name="restaurant_name" id="restaurant_name" type="text" class="validate" required="required">
-			          	<label for="restaurant_name">Nom du restaurant</label>
-			        </div>
-
-			        <div class="input-field col s8">
-			        	<i class="ion-ios-location-outline prefix"></i>
-			          	<input name="adress" id="adress" type="text" class="validate" required="required">
-			          	<label for="adress">Adresse</label>
-			        </div>
-
-			        <div class="input-field col s4">
-			          	<input name="post_code" id="post_code" type="number" maxlength="5" pattern="^(([0-8][0-9])|(9[0-5])|(2[ab]))[0-9]{3}$" class="validate" required="required">
-			          	<label id="post_code" for="post_code" data-error="Code postal invalide">Code postal</label>
-			        </div>
-
-				</div>
-
-			    <h4 id="contact">Que pouvons-nous faire pour vous ?</h4>
-			    <div class="row">
-				    <div class="input-field col s12"> 	
-				     	<i class="ion-ios-compose-outline prefix"></i>
-				        <textarea name="message" id="message" type="text" class="materialize-textarea" required="required" ></textarea>
-				        <label for="message">Dites-nous tout :)</label>
-				    </div>
-				</div>
-
-			    <div class="row">	
-					<div class="col s12 small-margin">	
-						<div align="center" class="g-recaptcha" data-sitekey="6LdwqgkUAAAAADJhz_RcoTKTdEnYw8mC1IuXuHeA"></div>
-					</div>
-
-					<div class="col s12">
-						<div class="row align-center small-margin">
-							<!-- <button class="btn waves-effect waves-light" type="submit" name="submit" value="sumibt">Envoyer</button> -->
-							<input class="btn waves-effect waves-light" type="submit" name="submit" value="Envoyer">
-						</div>
-					</div>
-				</div>
-				
-				<!-- Captcha Google -->
-				<?php
-					if(isset($_POST['submit']) && !empty($_POST['submit'])):
-					    if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])):
-					        //your site secret key
-					        $secret = '6LdwqgkUAAAAAOG7g2oLvZ7D9y7moXhtf5jjmqlK';
-							// On récupère l'IP de l'utilisateur
-							$remoteip = $_SERVER['REMOTE_ADDR'];
-					        //get verify response data
-					        $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-response']);
-					        $responseData = json_decode($verifyResponse);
-					        if($responseData->success):
-					            //contact form submission code
-					            $first_name = !empty($_POST['first_name'])?$_POST['first_name']:'';
-					        	$last_name = !empty($_POST['last_name'])?$_POST['last_name']:'';
-					            $telephone = !empty($_POST['telephone'])?$_POST['telephone']:'';
-					            $email = !empty($_POST['email'])?$_POST['email']:'';
-					            $restaurant_name = !empty($_POST['restaurant_name'])?$_POST['restaurant_name']:'';
-					            $adress = !empty($_POST['adress'])?$_POST['adress']:'';
-					            $post_code = !empty($_POST['post_code'])?$_POST['post_code']:'';
-					            $message = !empty($_POST['message'])?$_POST['message']:'';
-					            
-					            $to = 'contact@unowr.fr';
-					            $subject = 'New contact form have been submitted';
-					            $htmlContent = "
-					                <h2>Contact request details</h2>
-					                <h3>À propos du restaurateur : </h3>
-					                <p>
-					                <b>Prénom : </b>".$first_name."<br>
-					                <b>Nom : </b>".$last_name."<br>
-									<b>Téléphone : </b>".$telephone."<br>
-					                <b>Email : </b>".$email."<br>
-									<h3>À propos du restaurant : </h3>
-									<p><b>Nom du restaurant : </b>".$restaurant_name."<br>
-									<b>Adresse : </b>".$adress."<br></p>
-									<b>Code postal : </b>".$post_code."<br></p>
-					                <p><b>Message : </b>".$message."<br></p>
-					            ";
-					            // Always set content-type when sending HTML email
-					            $headers = "MIME-Version: 1.0" . "\r\n";
-					            $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-					            // More headers
-					            $headers .= 'From : '.$first_name.' '.$last_name.' <'.$email.'>' . "\r\n";
-					            //send email
-					            @mail($to,$subject,$htmlContent,$headers);
-					            
-					            $succMsg = 'Votre message a bien été envoyé !';
-					            echo '<p style="color: #26a69a"><b>';
-					            echo $succMsg;
-					            echo "</b></p>";
-					        else:
-					            $errMsg = 'Échec de vérification du captcha, essayez à nouveau...';
-					            echo '<p style="color: #F44336; text-align: center"><b>';
-					            echo $errMsg;
-					            echo "</b></p>";
-					        endif;
-					    else:
-					        $errMsg = "Pensez à valider le captcha avant d'envoyer votre message.";
-								echo '<p style="color: #F44336"; text-align: center><b>';
-					            echo $errMsg;
-					            echo "</b></p>";
-					    endif;
-					else:
-					    $errMsg = '';
-					    $succMsg = '';
-					endif;
-				?>
-	    	</form>
-			<!-- form -->
-    	</div>
-  	</div>
+  	<script>
+		$(document).ready(function() {
+    		$('select').material_select();
+  		});
+        
+        $('select').material_select('destroy');
+  	</script>

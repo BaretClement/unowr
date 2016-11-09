e6b706bef57e2975a84f736a0686a16e14062121
Clément Baret <clement.baret@gmail.com>
2016-11-09 17:28:16 +0100

    Update homepage template

--- wp-content/themes/minnow-child/template-homepage-material.php
+++ wp-content/themes/minnow-child/template-homepage-material.php
@@ -78,10 +78,10 @@ get_header( 'custom-material' ); ?>
 				<h2><?php the_field('titre_social'); ?></h2>
 				<p class="margin-bottom"><?php the_field('sous_titre_social'); ?></p>
 
-					<a href="<?php the_field('lien_social_1'); ?>" class="picto-social <?php the_field('pictogramme_social_1'); ?>"></a>
-					<a href="<?php the_field('lien_social_2'); ?>" class="picto-social <?php the_field('pictogramme_social_2'); ?>"></a>
-					<a href="<?php the_field('lien_social_3'); ?>" class="picto-social <?php the_field('pictogramme_social_3'); ?>"></a>
-					<a href="<?php the_field('lien_social_4'); ?>" class="picto-social <?php the_field('pictogramme_social_4'); ?>"></a>
+					<a class="<?php the_field('couleur_texte_social') ?>" href="<?php the_field('lien_social_1'); ?>" class="picto-social <?php the_field('pictogramme_social_1'); ?>"></a>
+					<a class="<?php the_field('couleur_texte_social') ?>" href="<?php the_field('lien_social_2'); ?>" class="picto-social <?php the_field('pictogramme_social_2'); ?>"></a>
+					<a class="<?php the_field('couleur_texte_social') ?>" href="<?php the_field('lien_social_3'); ?>" class="picto-social <?php the_field('pictogramme_social_3'); ?>"></a>
+					<a class="<?php the_field('couleur_texte_social') ?>" href="<?php the_field('lien_social_4'); ?>" class="picto-social <?php the_field('pictogramme_social_4'); ?>"></a>
 			</div>
 		</div>
 	</div>

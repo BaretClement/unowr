b30a0d2ba38a5f806e1190500ab7e00f12c4ea55
Clément Baret <clement.baret@gmail.com>
2016-11-10 17:15:21 +0100

    Update template

--- wp-content/themes/minnow-child/template-homepage-material.php
+++ wp-content/themes/minnow-child/template-homepage-material.php
@@ -32,7 +32,7 @@ get_header( 'custom-material' ); ?>
 	<!-- BLOC CONCEPT  -->
 	<div id="concept" class="row <?php the_field('couleur_texte_concept') ?>"" style="background-color: <?php the_field('background_concept'); ?>; background-image: url(<?php the_field('background_image_concept'); ?>);">
 
-		<div id="blog" class="col s12 m6 height75-img valign-wrapper"></div>
+		<div class="col s12 m6 height75-img valign-wrapper"></div>
 
 		<div class="col s12 l6 height75 valign-wrapper">
 			<div id="concept-text" class="container valign center">

660429921295b78c76ec3cc3384e9eb43b1e2b39
Clément Baret <clement.baret@gmail.com>
2016-11-12 16:25:36 +0100

    template recherche

--- wp-content/themes/minnow-child/template-recherche-material.php
+++ wp-content/themes/minnow-child/template-recherche-material.php
@@ -5,9 +5,11 @@ Template Name: Recherche - material
 
 get_header( 'custom-material' ); ?> 
 
+<?php the_field(formulaire_de_recherche); ?>
 <div class="row">
 	<div class="col s12 height100">
 		<div class="container">
+						
 
 			  <!-- Modal Trigger -->
 			  <a class="waves-effect waves-light btn" href="#modal1">Modal</a>
@@ -16,7 +18,7 @@ get_header( 'custom-material' ); ?>
 			  <div id="modal1" class="modal">
 			    <div class="modal-content">
 			      <h4>Modal Header</h4>
-					<?php the_field(formulaire_de_recherche); ?>	
+					<?php the_field(formulaire_de_reservation); ?>	
 			    </div>
 			    <div class="modal-footer">
 			      <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Agree</a>

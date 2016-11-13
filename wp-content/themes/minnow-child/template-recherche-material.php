5424fb602fa30708e9adebdb699308bdf697dcc9
Clément Baret <clement.baret@gmail.com>
2016-11-13 13:40:58 +0100

    Restaure modal but do not show it

--- wp-content/themes/minnow-child/template-recherche-material.php
+++ wp-content/themes/minnow-child/template-recherche-material.php
@@ -10,8 +10,22 @@ get_header( 'custom-material' ); ?>
 	<div class="col s12 height100">
 		<div class="container">
 						
-			<?php the_field(formulaire_de_recherche); ?>
-			  
+		<?php the_field(formulaire_de_recherche); ?>
+		
+			  <!-- Modal Trigger -->
+			  <a class="waves-effect waves-light btn" href="#modal1" style="display: none">Modal</a>
+
+			  <!-- Modal Structure -->
+			  <div id="modal1" class="modal">
+			    <div class="modal-content">
+			      <h4>Modal Header</h4>
+					<?php the_field(formulaire_de_reservation); ?>	
+			    </div>
+			    <div class="modal-footer">
+			      <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+			    </div>
+			  </div>
+
 		</div>
   	</div>
 </div>

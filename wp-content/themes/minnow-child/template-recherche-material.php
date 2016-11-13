f32517a345c29fac7e4963e643e6841fa4eebb2a
Clément Baret <clement.baret@gmail.com>
2016-11-13 14:28:03 +0100

    screen size

--- wp-content/themes/minnow-child/template-recherche-material.php
+++ wp-content/themes/minnow-child/template-recherche-material.php
@@ -7,7 +7,7 @@ get_header( 'custom-material' ); ?>
 
 
 <div class="row">
-	<div class="col s12 height100">
+	<div class="col s12 min-height100">
 		<div class="container">
 						
 		<?php the_field(formulaire_de_recherche); ?>

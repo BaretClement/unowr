4bb65b8f9453202cbfe07eeaa261a2318709c4c3
Clément Baret <clement.baret@gmail.com>
2016-11-13 20:46:27 +0100

    Reveal effect

--- wp-content/themes/minnow-child/template-homepage-material.php
+++ wp-content/themes/minnow-child/template-homepage-material.php
@@ -7,7 +7,7 @@ get_header( 'custom-material' ); ?>
 
 	<!-- BLOC UTILISATEUR -->
 	<div id="utilisateur" class="row" style="color: <?php the_field('couleur_texte_utilisateur') ?>; background-color: <?php the_field('background_utilisateur'); ?>; background-image: url(<?php the_field('background_image_utilisateur'); ?>);">
-		<div class="col s12 height100 valign-wrapper">
+		<div class="col s12 height100 valign-wrapper reveal-bottom">
 			<div class="container valign center">
 				<h1><?php the_field('titre_utilisateur'); ?></h1>
 				<p><?php the_field('sous_titre_utilisateur'); ?></p>
@@ -21,7 +21,7 @@ get_header( 'custom-material' ); ?>
 
 	<!-- BLOC RESTAURATEUR -->
 	<div class="row" style="color: <?php the_field('couleur_texte_restaurant') ?>; background-color: <?php the_field('background_restaurateur'); ?>; background-image: url(<?php the_field('background_image_restaurateur'); ?>);">
-		<div class="col s12 height100 valign-wrapper">
+		<div class="col s12 height100 valign-wrapper reveal-left">
 			<div class="container valign center">
 				<h2><?php the_field('titre_restaurateur'); ?></h2>
 				<p class="margin-bottom"><?php the_field('sous_titre_restaurateur'); ?></p>
@@ -59,7 +59,7 @@ get_header( 'custom-material' ); ?>
 
 		<div class="col s12 m6 height75-img valign-wrapper" style="background-image: url(<?php the_field('background_image_concept'); ?>); background-repeat: no-repeat; background-position: center; background-size: contain;"></div>
 
-		<div class="col s12 m6 height75 valign-wrapper">
+		<div class="col s12 m6 height75 valign-wrapper reveal-right">
 			<div id="concept-text" class="container valign center">
 				<h2><?php the_field('titre_concept'); ?></h2>
 				<p><?php the_field('sous_titre_concept'); ?></p>
@@ -77,7 +77,7 @@ get_header( 'custom-material' ); ?>
 
 		<div class="col s12 m6 push-m6 height75-img valign-wrapper" style="background-image: url(<?php the_field('background_image_blog'); ?>); background-repeat: no-repeat; background-position: center; background-size: contain;"></div>
 
-		<div class="col s12 m6 pull-m6 height75 valign-wrapper">
+		<div class="col s12 m6 pull-m6 height75 valign-wrapper reveal-left">
 			<div id="blog-text" class="container valign center">
 				<h2><?php the_field('titre_blog'); ?></h2>
 				<p><?php the_field('sous_titre_blog'); ?></p>
@@ -91,7 +91,7 @@ get_header( 'custom-material' ); ?>
 
 	<!-- BLOC SOCIAL -->
 	<div class="row" style="color: <?php the_field('couleur_texte_social') ?>; background-color: <?php the_field('background_social'); ?>; background-image: url(<?php the_field('background_image_social'); ?>);">
-		<div class="col s12 height50 valign-wrapper">
+		<div class="col s12 height50 valign-wrapper reveal-right">
 			<div class="container center valign">	
 				<h2><?php the_field('titre_social'); ?></h2>
 				<p class="margin-bottom"><?php the_field('sous_titre_social'); ?></p>
@@ -107,4 +107,12 @@ get_header( 'custom-material' ); ?>
 
 
 	</div> 
+	<script>
+	window.sr = ScrollReveal({duration: 2000});
+sr.reveal('.reveal', {origin: 'center', distance: '100%', scale: 1,});
+sr.reveal('.reveal-left', {origin: 'left', distance: '100%', scale: 1,});
+sr.reveal('.reveal-right', {origin: 'right', distance: '100%', scale: 1,});
+sr.reveal('.reveal-top', {origin: 'top', distance: '100%', scale: 1,});
+sr.reveal('.reveal-bottom', {origin: 'bottom', distance: '100%', scale: 1,});
+	</script>
     <?php get_footer( 'custom-material' ); ?>

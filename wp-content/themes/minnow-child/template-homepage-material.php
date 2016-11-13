9f535e86fcd183af42886236486f543ad7f5feb8
Clément Baret <clement.baret@gmail.com>
2016-11-13 20:26:08 +0100

    add color picker

--- wp-content/themes/minnow-child/template-homepage-material.php
+++ wp-content/themes/minnow-child/template-homepage-material.php
@@ -6,21 +6,21 @@ Template Name: Homepage - material
 get_header( 'custom-material' ); ?> 
 
 	<!-- BLOC UTILISATEUR -->
-	<div id="utilisateur" class="row <?php the_field('couleur_texte_utilisateur') ?>" style="background-color: <?php the_field('background_utilisateur'); ?>; background-image: url(<?php the_field('background_image_utilisateur'); ?>);">
+	<div id="utilisateur" class="row" style="color: <?php the_field('couleur_texte_utilisateur') ?>; background-color: <?php the_field('background_utilisateur'); ?>; background-image: url(<?php the_field('background_image_utilisateur'); ?>);">
 		<div class="col s12 height100 valign-wrapper">
 			<div class="container valign center">
 				<h1><?php the_field('titre_utilisateur'); ?></h1>
 				<p><?php the_field('sous_titre_utilisateur'); ?></p>
-				  <a href="<?php the_field('link_cta_utilisateur'); ?>" class="waves-effect waves-teal btn-flat <?php the_field('couleur_texte_utilisateur') ?>">
+				  <a href="<?php the_field('link_cta_utilisateur'); ?>" class="waves-effect waves-teal btn-flat" style="color: <?php the_field('couleur_texte_utilisateur') ?>">
 				  	<?php the_field('cta_utilisateur'); ?>
 				  </a>
-				  <div><i class="ion-ios-arrow-down <?php the_field('couleur_texte_utilisateur') ?>" style="font-size: 2rem"></i></div>
+				  <div><i class="ion-ios-arrow-down" style="color: <?php the_field('couleur_texte_utilisateur') ?>; font-size: 2rem"></i></div>
 			</div>
 		</div>
 	</div>
 
 	<!-- BLOC RESTAURATEUR -->
-	<div class="row <?php the_field('couleur_texte_restaurant') ?>" style="background-color: <?php the_field('background_restaurateur'); ?>; background-image: url(<?php the_field('background_image_restaurateur'); ?>);">
+	<div class="row" style="color: <?php the_field('couleur_texte_restaurant') ?>; background-color: <?php the_field('background_restaurateur'); ?>; background-image: url(<?php the_field('background_image_restaurateur'); ?>);">
 		<div class="col s12 height100 valign-wrapper">
 			<div class="container valign center">
 				<h2><?php the_field('titre_restaurateur'); ?></h2>
@@ -43,7 +43,7 @@ get_header( 'custom-material' ); ?>
     </div>
   </div>
 
-  <div><i class="ion-ios-arrow-down <?php the_field('couleur_texte_restaurant') ?>" style="font-size: 2rem"></i></div>
+  <div><i class="ion-ios-arrow-down" style="color: <?php the_field('couleur_texte_restaurant') ?>; font-size: 2rem"></i></div>
 
 
 
@@ -55,7 +55,7 @@ get_header( 'custom-material' ); ?>
 	</div>
 
 	<!-- BLOC CONCEPT  -->
-	<div id="concept" class="row <?php the_field('couleur_texte_concept') ?>"" style="background-color: <?php the_field('background_concept'); ?>;">
+	<div id="concept" class="row" style="color: <?php the_field('couleur_texte_concept') ?>; background-color: <?php the_field('background_concept'); ?>;">
 
 		<div class="col s12 m6 height75-img valign-wrapper" style="background-image: url(<?php the_field('background_image_concept'); ?>); background-repeat: no-repeat; background-position: center; background-size: contain;"></div>
 
@@ -63,16 +63,17 @@ get_header( 'custom-material' ); ?>
 			<div id="concept-text" class="container valign center">
 				<h2><?php the_field('titre_concept'); ?></h2>
 				<p><?php the_field('sous_titre_concept'); ?></p>
-				  <a href="<?php the_field('link_cta_concept'); ?>" class="waves-effect waves-teal btn-flat <?php the_field('couleur_texte_concept') ?>">
+				  <a href="<?php the_field('link_cta_concept'); ?>" class="waves-effect waves-teal btn-flat" style="color: <?php the_field('couleur_texte_concept') ?>">
 				  	<?php the_field('cta_concept'); ?>
 				  </a>
-				  <div><i class="ion-ios-arrow-down <?php the_field('couleur_texte_concept') ?>" style="font-size: 2rem"></i></div>
+				  <div><i class="ion-ios-arrow-down" style="color: <?php the_field('couleur_texte_concept') ?>; font-size: 2rem"></i></div>
 			</div>
 		</div>
 	</div>
 
+
 	<!-- BLOC BLOG  -->
-	<div class="row <?php the_field('couleur_texte_blog') ?>" style="background-color: <?php the_field('background_blog'); ?>;">
+	<div class="row" style="color: <?php the_field('couleur_texte_blog') ?>; background-color: <?php the_field('background_blog'); ?>;">
 
 		<div class="col s12 m6 push-m6 height75-img valign-wrapper" style="background-image: url(<?php the_field('background_image_blog'); ?>); background-repeat: no-repeat; background-position: center; background-size: contain;"></div>
 
@@ -83,13 +84,13 @@ get_header( 'custom-material' ); ?>
 				  <a href="<?php the_field('link_cta_blog'); ?>" style="margin-bottom: 25px;" class="waves-effect waves-teal btn disabled <?php the_field('couleur_texte_blog') ?>">
 				  	<?php the_field('cta_blog'); ?>
 				  </a>
-				  <div><i class="ion-ios-arrow-down <?php the_field('couleur_texte_blog') ?>" style="font-size: 2rem"></i></div>
+				  <div><i class="ion-ios-arrow-down" style="color: <?php the_field('couleur_texte_blog') ?>; font-size: 2rem"></i></div>
 			</div>
 		</div>
 	</div>
 
 	<!-- BLOC SOCIAL -->
-	<div class="row <?php the_field('couleur_texte_social') ?>" style="background-color: <?php the_field('background_social'); ?>; background-image: url(<?php the_field('background_image_social'); ?>);">
+	<div class="row" style="color: <?php the_field('couleur_texte_social') ?>; background-color: <?php the_field('background_social'); ?>; background-image: url(<?php the_field('background_image_social'); ?>);">
 		<div class="col s12 height50 valign-wrapper">
 			<div class="container center valign">	
 				<h2><?php the_field('titre_social'); ?></h2>

c9d0331a83656c747bddf28e21322f29904a74ad
Clément Baret <clement.baret@gmail.com>
2016-11-13 13:37:51 +0100

    Delete modal search page

--- wp-content/themes/minnow-child/template-recherche-material.php
+++ wp-content/themes/minnow-child/template-recherche-material.php
@@ -5,25 +5,13 @@ Template Name: Recherche - material
 
 get_header( 'custom-material' ); ?> 
 
-<?php the_field(formulaire_de_recherche); ?>
+
 <div class="row">
 	<div class="col s12 height100">
 		<div class="container">
 						
-			  <!-- Modal Trigger -->
-			  <a class="waves-effect waves-light btn" href="#modal1">Modal</a>
-
-			  <!-- Modal Structure -->
-			  <div id="modal1" class="modal">
-			    <div class="modal-content">
-			      <h4>Modal Header</h4>
-					<?php the_field(formulaire_de_reservation); ?>	
-			    </div>
-			    <div class="modal-footer">
-			      <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
-			    </div>
-			  </div>
-
+			<?php the_field(formulaire_de_recherche); ?>
+			  
 		</div>
   	</div>
 </div>

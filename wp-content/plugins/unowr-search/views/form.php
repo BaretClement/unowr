1e4d4674f6a8e0ff84a55b7a8c29e543c30f8716
Clément Baret <clement.baret@gmail.com>
2016-11-14 17:22:28 +0100

    Custom user answers

--- wp-content/plugins/unowr-search/views/form.php
+++ wp-content/plugins/unowr-search/views/form.php
@@ -98,11 +98,13 @@ foreach ($taxonomies as $tkey => $taxonomy) {
 				. $term->term_id . '" data-unowr-taxonomy="' . $taxonomy . '">' . $term->name . '</div>';
 		}
 	}
-	echo '<div id="triangle-right"></div><div class="user-avatar"></div></p></div></div>';
+	echo '<div id="triangle-right"></div><div class="user-avatar"></div>';
 
 	foreach ($parentHtml as $key => $value) {
 		echo "<div class='children unowr-hidden' data-parent='" . $key . "'>";
 	?>
+	</p></div></div>
+	
 	<div>
 		<div class="row valign-wrapper search">
 			<div class="bot-avatar"></div>

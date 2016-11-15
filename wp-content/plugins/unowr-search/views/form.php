9ede6d7f27fdd101c03dba3c2368d98e2d241a73
Clément Baret <clement.baret@gmail.com>
2016-11-15 19:16:54 +0100

    Try changes form.php

--- wp-content/plugins/unowr-search/views/form.php
+++ wp-content/plugins/unowr-search/views/form.php
@@ -97,7 +97,7 @@ foreach ($taxonomies as $tkey => $taxonomy) {
 				. $term->term_id . '" data-unowr-taxonomy="' . $taxonomy . '">' . $term->name . '</div>';
 		}
 	}
-	echo '<div id="triangle-right"></div><div class="user-avatar"></div></div>';
+	echo '</div></div><div id="triangle-right"></div><div class="user-avatar">';
 
 	foreach ($parentHtml as $key => $value) {
 		echo "<div class='children unowr-hidden' data-parent='" . $key . "'>";

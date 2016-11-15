7aeb8bd6154b83b143daefa8169f2b405d4b756e
Clément Baret <clement.baret@gmail.com>
2016-11-15 13:44:58 +0100

    Style results

--- wp-content/plugins/unowr-search/js/script.js
+++ wp-content/plugins/unowr-search/js/script.js
@@ -92,7 +92,7 @@ var unowr_form = {
 	_finish: function () {
 		var html = "<div class='row valign-wrapper search'><div class='bot-avatar'></div><div id='triangle-left'></div><p class='questions search'>Je peux te proposer les trois restaurants suivants :</p></div>";
 		Array.prototype.forEach.call(this._resto, (resto) => {
-			html += "<div class='row valign-wrapper search'><div class='unowr_result'>";
+			html += "<div class='row valign-wrapper search'><div class='unowr_result'><p class='questions search'>";
 			html += "	<p class='result-id question search' data-value='" + resto.id + "'></p>";
 			html += "	<span class='result-title' data-value='" + escape(resto.title) + "'>Nom : " + resto.title + "</span><br>";
 			html += "	<span class='result-prix_moyen' data-value='" + resto.prix_moyen + "'>Prix : " + resto.prix_moyen + "</span><br>";
@@ -100,7 +100,7 @@ var unowr_form = {
 			html += "	<span class='result-subcategory' data-value='" + resto.category + ", " + resto.subcategory + "'>Type de cuisine : " + resto.category + ", " + resto.subcategory + "</span><br>";
 			html += "	<span class='result-ambiances' data-value='" + resto.ambiances.join(', ') + "'>Ambiances : " + resto.ambiances.join(', ') + "</span><br>";
 			html += "<button class='btn-book-resto'>Réserver</button>";
-			html += "</div></div>";
+			html += "</p></div></div>";
 		})
 
 		this._resultWrapper.innerHTML = html

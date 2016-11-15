99995b8bc23e6d27b69d86b63a908d32fb286058
Clément Baret <clement.baret@gmail.com>
2016-11-15 14:32:49 +0100

    Style results

--- wp-content/plugins/unowr-search/js/script.js
+++ wp-content/plugins/unowr-search/js/script.js
@@ -97,7 +97,7 @@ var unowr_form = {
 			html += "	<span class='result-title' data-value='" + escape(resto.title) + "'>Nom : " + resto.title + "</span><br>";
 			html += "	<span class='result-prix_moyen' data-value='" + resto.prix_moyen + "'>Prix : " + resto.prix_moyen + "</span><br>";
 			html += "	<span class='result-adresse' data-value='" + escape(resto.adresse) + "'>Adresse : " + resto.adresse + "</span><br>";
-			html += "	<div class='chip'><p class='result-subcategory' data-value='" + resto.category + ", " + resto.subcategory + ", " + resto.category + "</div>, <div class='chip'>" + resto.subcategory + "</div></p><br>";
+			html += "	<span class='result-subcategory' data-value='" + resto.category + ", " + resto.subcategory + ", " + resto.category + "</div>, " + resto.subcategory + "</span><br>";
 			html += "	<span class='result-ambiances' data-value='" + resto.ambiances.join(', ') + "'>Ambiances : " + resto.ambiances.join(', ') + "</span><br>";
 			html += "<button class='btn-book-resto'>Réserver</button>";
 			html += "</p></div></div>";

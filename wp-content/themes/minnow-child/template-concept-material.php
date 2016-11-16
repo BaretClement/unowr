126b8ab9cce9425b609a44c457a2e8a4ee5db4fa
Clément Baret <clement.baret@gmail.com>
2016-11-16 16:45:17 +0100

    Design page concept

--- wp-content/themes/minnow-child/template-concept-material.php
+++ wp-content/themes/minnow-child/template-concept-material.php
@@ -10,7 +10,7 @@ get_header( 'custom-material' ); ?>
     <div class="col s12 min-height100 valign-wrapper">
       <div class="container valign center">
         <h1>
-          <span style="background: rgba(FFF, FFF, FFF, 0.2)">
+          <span style="background: rgba(255, 255, 255, 0.3)">
             <?php the_field('titre_unowr'); ?>
           </span>
         </h1> 

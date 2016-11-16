be423b86fe9f75c0e04b89b91e9769e8f12106d3
Clément Baret <clement.baret@gmail.com>
2016-11-16 16:28:13 +0100

    Design page concept

--- wp-content/themes/minnow-child/js/script.js
+++ wp-content/themes/minnow-child/js/script.js
@@ -3,10 +3,3 @@
     $('.modal').modal();
   });
           
-$(document).on('click', 'a', function(event){
-    event.preventDefault();
-
-    $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 500);
-});

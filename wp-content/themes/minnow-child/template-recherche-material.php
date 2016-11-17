24784a78728672d04a89bcedc44233a77c335f99
Clément Baret <clement.baret@gmail.com>
2016-11-17 15:12:23 +0100

    Fix display modal

--- wp-content/themes/minnow-child/template-recherche-material.php
+++ wp-content/themes/minnow-child/template-recherche-material.php
@@ -32,7 +32,7 @@ get_header( 'custom-material' ); ?>
 
 
 <!-- Modal Structure -->                    
-<div id="modal-reservation" class="modal modal-fixed-footer">
+<div id="modal-reservation" class="modal modal">
 <div class="modal-content">
 <h4>Demande de réservation</h4>
 <p><?php the_field('formulaire_de_reservation'); ?></p> 

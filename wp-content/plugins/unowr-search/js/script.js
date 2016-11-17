130482e88f4bda84049829d25ee76bb19c3814ed
Clément Baret <clement.baret@gmail.com>
2016-11-17 20:02:12 +0100

    Add class to target choice

--- wp-content/plugins/unowr-search/js/script.js
+++ wp-content/plugins/unowr-search/js/script.js
@@ -66,6 +66,13 @@ var unowr_form = {
 	_next: function (e) {
 		var target = e.currentTarget
 
+
+var others = target.parentNode.querySelectorAll('.unowr-choice')
+Array.prototype.forEach.call(others, (other) => {
+other.classList.add('hidden')
+})
+target.classList.add(‘active’);
+
 		var taxo = target.getAttribute('data-unowr-taxonomy')
 		var parent = target.getAttribute('data-parent')
 		var parentNode = document.querySelector('[data-unowr-taxonomy="' + taxo + '"]')
